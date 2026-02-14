f23227530eb4687d9ba7656b223c150985d264d6
milochen0418 <milo_chen@mail2000.com.tw>
2026-02-15 04:45:28 +0800

    update rxconfig

--- rxconfig.py
+++ rxconfig.py
@@ -1,3 +1,10 @@
 import reflex as rx
 
-config = rx.Config(app_name="video_duration_adjuster", plugins=[rx.plugins.TailwindV3Plugin()])
+config = rx.Config(
+    app_name="video_duration_adjuster",
+    api_url="http://localhost:8000",
+    plugins=[
+        rx.plugins.TailwindV4Plugin(),
+        rx.plugins.SitemapPlugin(),
+    ],
+)
